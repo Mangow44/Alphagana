feab5d3c382f4d9b4fca96234aa6ce3b676f42cf
Thomas Clouet <thomas.clouet1@etu.univ-nantes.fr>
2023-07-19 19:16:45 +0200

    :sparkles: feat(header): implement header

--- src/components/header/CustomHeader.vue
+++ src/components/header/CustomHeader.vue
@@ -2,6 +2,19 @@
 
 <template>
   <header>
-    <h1>ALPHAGANA</h1>
+    <h1 class="title">ALPHAGANA</h1>
   </header>
 </template>
+
+<style scoped>
+header {
+  display: flex;
+
+  padding: 0.4rem 0;
+}
+header .title {
+  margin: auto;
+
+  font-size: 1.3rem;
+}
+</style>
