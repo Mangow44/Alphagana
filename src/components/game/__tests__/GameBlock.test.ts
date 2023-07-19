8e4a951154c7381713a13620094643acf0378168
Thomas Clouet <61280720+Mangow44@users.noreply.github.com>
2023-07-19 22:02:39 +0200

    Feature/homeview (#3)
    * :sparkles: feat(homeview): create game
    
    * :zap: feat/test(game block): create game block and add tests

---
+++ src/components/game/__tests__/GameBlock.test.ts
@@ -0,0 +1,77 @@
+import { beforeEach, describe, expect, it, vi } from 'vitest'
+import { shallowMount } from '@vue/test-utils'
+import GameBlock from '@/components/game/GameBlock.vue'
+
+describe('Game block', () => {
+  const guess: string = 'さようなら'
+  const translation: string = 'au revoir'
+  const answer: string = 'sayounara'
+  const wrapper: any = shallowMount(GameBlock, {
+    props: {
+      guess,
+      translation,
+      answer
+    }
+  })
+
+  const guessInputDataTestId: string = "[data-testid='guess-input']"
+  const validateButtonDataTestId: string = "[data-testid='validate-btn']"
+  const feedbackErrorDataTestId: string = "[data-testid='fb-error']"
+
+  beforeEach(() => {
+    vi.resetAllMocks()
+  })
+
+  it('User input is correct', async () => {
+    // Given
+    const guessInput = wrapper.find(guessInputDataTestId)
+    const validateButton = wrapper.find(validateButtonDataTestId)
+
+    // When
+    guessInput.setValue(answer)
+    await validateButton.trigger('click')
+
+    // Expect
+    expect(wrapper.emitted('generateNewGuess')).toBeTruthy()
+  })
+
+  it('User input is incorrect', async () => {
+    // Given
+    const guessInput = wrapper.find(guessInputDataTestId)
+    const validateButton = wrapper.find(validateButtonDataTestId)
+    const feedbackError = wrapper.find(feedbackErrorDataTestId)
+
+    // When
+    guessInput.setValue('nani')
+    await validateButton.trigger('click')
+
+    // Expect
+    expect(wrapper.vm.feedbackError).toEqual(answer)
+    expect(feedbackError.classes().includes('display')).toBeTruthy()
+  })
+
+  it('Reset feedback and user input when props change', async () => {
+    // Given
+    const guessInput = wrapper.find(guessInputDataTestId)
+    const validateButton = wrapper.find(validateButtonDataTestId)
+
+    // When
+    guessInput.setValue('nani')
+    await validateButton.trigger('click')
+
+    // Then
+    expect(wrapper.vm.feedbackError).toEqual(answer)
+    expect(wrapper.vm.userGuessValue).toEqual('nani')
+
+    // When
+    await wrapper.setProps({
+      guess: 'ok',
+      translation: 'ok',
+      answer: 'ok'
+    })
+
+    // Then
+    expect(wrapper.vm.feedbackError).toBeNull()
+    expect(wrapper.vm.userGuessValue).toBeNull()
+  })
+})
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,7 +1,39 @@
-<script setup lang="ts"></script>
+<script setup lang="ts">
+import { ref } from 'vue'
+import { guesses } from '@/data/guesses'
+import Guess from '@/models/Guess'
+import GameBlock from '@/components/game/GameBlock.vue'
+
+const currentGuess = ref<Guess | null>(null)
+
+generateNewCurrentGuess()
+
+function generateNewCurrentGuess() {
+  currentGuess.value = guesses[getRandomNumberBetween(0, guesses.length - 1)]
+}
+
+function getRandomNumberBetween(min: number, max: number): number {
+  return Math.floor(Math.random() * (max - min + 1)) + min
+}
+</script>
 
 <template>
-  <main>home</main>
+  <main>
+    <game-block
+      v-if="currentGuess"
+      :guess="currentGuess.hiragana"
+      :translation="currentGuess.fr"
+      :answer="currentGuess.romaji"
+      @generate-new-guess="generateNewCurrentGuess()"
+    />
+  </main>
 </template>
 
-<style></style>
+<style scoped>
+main {
+  display: flex;
+
+  width: 100%;
+  height: 100%;
+}
+</style>
