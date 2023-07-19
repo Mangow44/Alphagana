35584815b918298f747397d0409a9d76df32e35e
Thomas Clouet <thomas.clouet1@etu.univ-nantes.fr>
2023-07-19 19:10:20 +0200

    :rocket: init project

---
+++ src/App.vue
@@ -0,0 +1,10 @@
+<script setup lang="ts">
+import { RouterView } from 'vue-router'
+import CustomHeader from '@/components/header/CustomHeader.vue'
+</script>
+
+<template>
+  <custom-header />
+
+  <RouterView />
+</template>
---
+++ src/components/header/CustomHeader.vue
@@ -0,0 +1,7 @@
+<script setup lang="ts"></script>
+
+<template>
+  <header>
+    <h1>ALPHAGANA</h1>
+  </header>
+</template>
