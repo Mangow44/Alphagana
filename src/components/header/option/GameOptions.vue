790ddd0102a34e414855e7722ab1ba72c74a52e2
Thomas Clouet <thomas.clouet1@etu.univ-nantes.fr>
2023-07-27 19:18:34 +0200

    :art: refactor(header, clickable icon): improve css/ui

--- src/components/header/option/GameOptions.vue
+++ src/components/header/option/GameOptions.vue
@@ -1,18 +1,19 @@
 <script setup lang="ts">
 import { ref } from 'vue'
+import ClickableIcon from '@/components/assets/ClickableIcon.vue'
 import OptionsPane from '@/components/header/option/OptionsPane.vue'
 
 const isOptionsPaneDisplayed = ref<boolean>(false)
 </script>
 
 <template>
-  <button
+  <clickable-icon
     class="btn-options"
     @click="isOptionsPaneDisplayed = !isOptionsPaneDisplayed"
+    :src="'/public/icons/gear.svg'"
+    :alt="'Options'"
     data-testid="btn-options"
-  >
-    <img src="@/assets/icons/gear.svg" alt="Options" />
-  </button>
+  />
 
   <options-pane
     v-if="isOptionsPaneDisplayed"
@@ -22,19 +23,6 @@ const isOptionsPaneDisplayed = ref<boolean>(false)
 
 <style scoped>
 .btn-options {
-  width: 2rem;
-  height: 2rem;
-  margin: auto 0;
   margin-right: 1rem;
-
-  cursor: pointer;
-
-  border: none;
-  background-color: transparent;
-}
-
-.btn-options img {
-  width: 100%;
-  height: 100%;
 }
 </style>
