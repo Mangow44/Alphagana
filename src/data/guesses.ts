4e907e65f308f10b6ce33491c242785ed8d7382b
Thomas Clouet <thomas.clouet1@etu.univ-nantes.fr>
2023-08-03 18:39:39 +0200

    :bug: fix(data): remove dupes and sort data

--- src/data/guesses.ts
+++ src/data/guesses.ts
@@ -4,6 +4,7 @@ export const guesses: Array<Guess> = [
   { hiragana: 'はい', romaji: 'hai', fr: 'oui', en: 'yes' },
   { hiragana: 'いいえ', romaji: 'iie', fr: 'non', en: 'no' },
 
+  // nature
   { hiragana: 'いぬ', romaji: 'inu', fr: 'chien', en: 'dog' },
   { hiragana: 'ねこ', romaji: 'neko', fr: 'chat', en: 'cat' },
   { hiragana: 'うさぎ', romaji: 'usagi', fr: 'lapin', en: 'rabbit' },
@@ -12,6 +13,7 @@ export const guesses: Array<Guess> = [
   { hiragana: 'とり', romaji: 'tori', fr: 'oiseau', en: 'bird' },
   { hiragana: 'とら', romaji: 'tora', fr: 'tigre', en: 'tiger' },
 
+  // pays
   { hiragana: 'あめりか', romaji: 'amerika', fr: 'états-unis', en: 'united states' },
   { hiragana: 'ふらんす', romaji: 'furansu', fr: 'france', en: 'france' },
   { hiragana: 'いぎりす', romaji: 'igirisu', fr: 'royaume-uni', en: 'united kingdom' },
@@ -21,12 +23,7 @@ export const guesses: Array<Guess> = [
   { hiragana: 'どいつ', romaji: 'doitsu', fr: 'allemagne', en: 'germany' },
   { hiragana: 'ぶらじる', romaji: 'burajiru', fr: 'brésil', en: 'brazil' },
 
-  { hiragana: 'うさぎ', romaji: 'usagi', fr: 'lapin', en: 'rabbit' },
-  { hiragana: 'らいおん', romaji: 'raion', fr: 'lion', en: 'lion' },
-  { hiragana: 'さかな', romaji: 'sakana', fr: 'poisson', en: 'fish' },
-  { hiragana: 'とり', romaji: 'tori', fr: 'oiseau', en: 'bird' },
-  { hiragana: 'とら', romaji: 'tora', fr: 'tigre', en: 'tiger' },
-
+  // couleurs
   { hiragana: 'あかい', romaji: 'akai', fr: 'rouge', en: 'red' },
   { hiragana: 'あおい', romaji: 'aoi', fr: 'bleu', en: 'blue' },
   { hiragana: 'みどり', romaji: 'midori', fr: 'vert', en: 'green' },
@@ -36,6 +33,7 @@ export const guesses: Array<Guess> = [
   { hiragana: 'くろい', romaji: 'kuroi', fr: 'noir', en: 'black' },
   { hiragana: 'しろい', romaji: 'shiroi', fr: 'blanc', en: 'white' },
 
+  // météo
   { hiragana: 'たいよう', romaji: 'taiyou', fr: 'soleil', en: 'sun' },
   { hiragana: 'あめ', romaji: 'ame', fr: 'pluie', en: 'rain' },
   { hiragana: 'つき', romaji: 'tsuki', fr: 'lune', en: 'moon' },
@@ -44,9 +42,11 @@ export const guesses: Array<Guess> = [
   { hiragana: 'あき', romaji: 'aki', fr: 'automne', en: 'autumn' },
   { hiragana: 'ふゆ', romaji: 'fuyu', fr: 'hiver', en: 'winter' },
 
+  // vétements
   { hiragana: 'どれす', romaji: 'doresu', fr: 'robe', en: 'dress' },
   { hiragana: 'くつ', romaji: 'kutsu', fr: 'chaussure', en: 'shoe' },
 
+  // nourriture
   { hiragana: 'ちいず', romaji: 'chiizu', fr: 'fromage', en: 'cheese' },
   { hiragana: 'おちゃ', romaji: 'ocha', fr: 'thé', en: 'tea' },
   { hiragana: 'おこのみやき', romaji: 'okonomiyaki', fr: 'crêpe salée', en: 'crepe (salty)' },
@@ -66,6 +66,7 @@ export const guesses: Array<Guess> = [
 
   { hiragana: 'ちょっと', romaji: 'chotto', fr: 'un petit peu', en: 'a little' },
 
+  // Salutations
   { hiragana: 'はじめまして', romaji: 'hajimemashite', fr: 'enchanté', en: 'nice to meet you' },
   { hiragana: 'おはよう', romaji: 'ohayou', fr: 'bonne matinée', en: 'good morning' },
   { hiragana: 'こんにちわ', romaji: 'konnichiwa', fr: 'bonjour', en: 'hello' },
@@ -76,6 +77,7 @@ export const guesses: Array<Guess> = [
   { hiragana: 'どこ', romaji: 'doko', fr: 'où', en: 'where' },
   { hiragana: 'どう', romaji: 'dou', fr: 'comment', en: 'how' },
 
+  // Professions
   { hiragana: 'こうこうせい', romaji: 'koukousei', fr: 'lycéen', en: 'high school student' },
   { hiragana: 'がくせい', romaji: 'gakusei', fr: 'étudiant', en: 'student' },
   { hiragana: 'かいしゃいん', romaji: 'kaishain', fr: 'employé', en: 'employee' },
